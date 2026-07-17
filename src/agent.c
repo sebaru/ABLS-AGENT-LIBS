9044c41e73de6d55a22bd6dd250382b788b3771e
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2026-07-17 19:10:29 +0200

    Enhance Agent_get_mqtt_api_message to preempt API commands for stopping, restarting, upgrading, and changing log levels

--- src/agent.c
+++ src/agent.c
@@ -44,10 +44,52 @@
 /******************************************************************************************************************************/
  JsonNode *Agent_get_mqtt_api_message ( struct ABLS_AGENT *agent )
   { if (!agent || !agent->mqtt_api) return(NULL);
-    return ( Mqtt_get_message ( agent->mqtt_api ) );
+    JsonNode *api_message = Mqtt_get_message ( agent->mqtt_api );
+    if (api_message)
+     { if ( Mqtt_topic_is ( api_message, 4, "+", "STOP", "AGENT", agent->agent_tech_id ) )
+        { Info( __func__, agent->agent_classe, agent->agent_tech_id, LOG_NOTICE, "Agent is stopping by API request" );
+          agent->Agent_run = AGENT_NEED_TO_STOP;
+        }
+       else if ( Mqtt_topic_is ( api_message, 4, "+", "RESTART", "AGENT", agent->agent_tech_id ) )
+        { Info( __func__, agent->agent_classe, agent->agent_tech_id, LOG_NOTICE, "Agent is restarting by API request" );
+          agent->Agent_run = AGENT_NEED_TO_RESTART;
+        }
+       else if ( Mqtt_topic_is ( api_message, 4, "+", "UPGRADE", "AGENT", agent->agent_tech_id )
+              || Mqtt_topic_is ( api_message, 4, "+", "UPGRADE", "CLASS", agent->agent_classe ) )
+        { Info( __func__, agent->agent_classe, agent->agent_tech_id, LOG_NOTICE, "Agent is upgrading by API request" );
+          gint new_pid = fork();
+          if (new_pid<0)
+           { Info( __func__, agent->agent_classe, agent->agent_tech_id, LOG_WARNING, "Fils: UPGRADE: Fork Error" ); }
+          else if (!new_pid)
+           { gchar chaine[256];
+             g_snprintf ( chaine, sizeof(chaine), "sudo dnf upgrade abls-agent-%s", agent->agent_classe );
+             system(chaine);
+             Info( __func__, agent->agent_classe, agent->agent_tech_id, LOG_WARNING, "Fils: UPGRADE: done. Restarting." );
+             agent->Agent_run = AGENT_NEED_TO_RESTART;                                                  /* Stop old processes */
+             exit(0);
+           }
+        }
+       else if ( Mqtt_topic_is ( api_message, 4, "+", "LOG", "AGENT", agent->agent_tech_id ) )
+        { if ( Json_has_member ( api_message, "log_level" ) )
+           { gint log_level = Json_get_int ( api_message, "log_level" );
+             Info_change_log_level ( log_level );
+           }
+          else if ( Json_has_member ( api_message, "debug" ) )
+           { gchar *facility = Json_get_string ( api_message, "debug" );
+             if (facility) Info_debug_facility ( agent->agent_tech_id, facility );
+           }
+          else if ( Json_has_member ( api_message, "undebug" ) )
+           { gchar *facility = Json_get_string ( api_message, "undebug" );
+             if (facility) Info_undebug_facility ( agent->agent_tech_id, facility );
+           }
+        }
+       else return ( api_message );                                      /* Transfert directement pour traitement par l'agent */
+     }
+    Json_unref ( api_message );                       /* Message traité en pre-emption par la librairie, on libère la mémoire */
+    return(NULL);
   }
 /******************************************************************************************************************************/
-/* Agent_get_mqtt_local_message: Dépile un message de la queue MQTT locale (non-bloquant)                                    */
+/* Agent_get_mqtt_local_message: Dépile un message de la queue MQTT locale (non-bloquant)                                     */
 /* Entrée: La structure afférente                                                                                             */
 /* Sortie: pointeur vers le JsonNode du message, ou NULL si aucun message disponible                                          */
 /******************************************************************************************************************************/
@@ -126,48 +168,6 @@
        Mqtt_Send_AI ( agent, agent->ai_nbr_tour_par_sec, agent->nbr_tour_par_sec, TRUE );
        Mqtt_Send_AI ( agent, agent->ai_log_par_min, 1.0*Info_reset_nbr_log(), TRUE );
        agent->telemetrie_next_update = time(NULL) + 60;
-     }
-/******************************************************** Ecoute API **********************************************************/
-    JsonNode *api_message = Mqtt_get_message ( agent->mqtt_api );
-    if (api_message)
-     { if ( Mqtt_topic_is ( api_message, 4, "+", "STOP", "AGENT", agent->agent_tech_id ) )
-        { Info( __func__, agent->agent_classe, agent->agent_tech_id, LOG_NOTICE, "Agent is stopping by API request" );
-          agent->Agent_run = AGENT_NEED_TO_STOP;
-        }
-       else if ( Mqtt_topic_is ( api_message, 4, "+", "RESTART", "AGENT", agent->agent_tech_id ) )
-        { Info( __func__, agent->agent_classe, agent->agent_tech_id, LOG_NOTICE, "Agent is restarting by API request" );
-          agent->Agent_run = AGENT_NEED_TO_RESTART;
-        }
-       else if ( Mqtt_topic_is ( api_message, 4, "+", "UPGRADE", "AGENT", agent->agent_tech_id )
-              || Mqtt_topic_is ( api_message, 4, "+", "UPGRADE", "CLASS", agent->agent_classe ) )
-        { Info( __func__, agent->agent_classe, agent->agent_tech_id, LOG_NOTICE, "Agent is upgrading by API request" );
-          gint new_pid = fork();
-          if (new_pid<0)
-           { Info( __func__, agent->agent_classe, agent->agent_tech_id, LOG_WARNING, "Fils: UPGRADE: Fork Error" ); }
-          else if (!new_pid)
-           { gchar chaine[256];
-             g_snprintf ( chaine, sizeof(chaine), "sudo dnf upgrade abls-agent-%s", agent->agent_classe );
-             system(chaine);
-             Info( __func__, agent->agent_classe, agent->agent_tech_id, LOG_WARNING, "Fils: UPGRADE: done. Restarting." );
-             agent->Agent_run = AGENT_NEED_TO_RESTART;                                                  /* Stop old processes */
-             exit(0);
-           }
-        }
-       else if ( Mqtt_topic_is ( api_message, 4, "+", "LOG", "AGENT", agent->agent_tech_id ) )
-        { if ( Json_has_member ( api_message, "log_level" ) )
-           { gint log_level = Json_get_int ( api_message, "log_level" );
-             Info_change_log_level ( log_level );
-           }
-          else if ( Json_has_member ( api_message, "debug" ) )
-           { gchar *facility = Json_get_string ( api_message, "debug" );
-             if (facility) Info_debug_facility ( agent->agent_tech_id, facility );
-           }
-          else if ( Json_has_member ( api_message, "undebug" ) )
-           { gchar *facility = Json_get_string ( api_message, "undebug" );
-             if (facility) Info_undebug_facility ( agent->agent_tech_id, facility );
-           }
-        }
-       Json_unref ( api_message );
      }
   }
 /******************************************************************************************************************************/
