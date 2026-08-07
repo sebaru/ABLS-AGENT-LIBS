1560e5118f0b0c67da7fdcf79db4132b17d0220f
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2026-08-07 07:12:47 +0200

    Update CMake build source directory and enhance user/group logging in agent initialization

--- src/agent.c
+++ src/agent.c
@@ -34,6 +34,7 @@
  #include <locale.h>
  #include <stdarg.h>
  #include <grp.h>
+ #include <pwd.h>
 
 /**************************************************** Prototypes de fonctions *************************************************/
  #include "abls-agent-libs.h"
@@ -136,10 +137,13 @@
  struct ABLS_AGENT *Agent_init ( gchar *entete, gchar *agent_classe, gchar *agent_version, gint sizeof_vars, int argc, char **argv )
   { gchar chaine[128];
     setlocale( LC_ALL, "C" );                                            /* Pour le formattage correct des , . dans les float */
+
     Info_init ( entete, "agent_tech_id", LOG_INFO );
     Info( __func__, agent_classe, NULL, LOG_INFO, "Agent of class '%s' (version %s) is starting with ABLS_AGENT_LIBS_VERSION=%s",
           agent_classe, agent_version, ABLS_AGENT_LIBS_VERSION );
-    Info( __func__, agent_classe, NULL, LOG_INFO, "User  '%s',  Group = '%s'", getlogin(), getgrgid(getgid())->gr_name );
+    Info( __func__, agent_classe, NULL, LOG_INFO, "User '%s', Group = '%s'",
+          getpwuid(getuid())->pw_name, getgrgid(getgid())->gr_name );
+
     struct ABLS_AGENT *agent = g_try_malloc0 ( sizeof(struct ABLS_AGENT) );
     if (!agent)
      { Info( __func__, agent_classe, NULL, LOG_ALERT, "Memory error trying to malloc struct ABLS_AGENT" );
